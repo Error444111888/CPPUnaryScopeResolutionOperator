7544deadad7c81a3cc4cc327ae8435b184961074
error418 <error418@hotmail.com>
2018-11-23 21:11:11 -0800

    Final Commit

--- CPPUnaryScopeResolutionOperator/Source.cpp
+++ CPPUnaryScopeResolutionOperator/Source.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
-
 using namespace std;
 
 
-double BTCPrice = 1000.11; // This is the Global variable
 
+double BTCPrice = 1000.111111443263545235; // This is the Global variable
 
 int main()
 {
+
+	cout.precision(10); // Setting the result to show 10 digits total
+
 	int BTCPrice = 5000; // This is the Local variable
 	cout << BTCPrice << endl;
 	cout << ::BTCPrice << endl; //<<< This calls the Global Variable instead of the local variable
